eaec7f84b44810a5504e2e5dbc7cd8da20356054
zhao <339842263zwl@gamil.com>
2016-06-25 20:24:36 +0800

    第一版

---
+++ src/FAQ.js
@@ -0,0 +1,129 @@
+var currentQuestion = 0;
+var score = 0;
+var submitBtn = document.getElementsByClassName('submit')[0];
+var askQuestion = true;
+var answerArea = document.getElementsByClassName('answerArea')[0];
+// var quizData;
+// fetch('/hunter/exam.json',{
+//     credentials: 'include',
+//     headers:{ "Content-Type": "application/json"}
+// }).
+// then(function(response) {
+//   return response.json();
+// }).then(function(data) {
+//   return quizData = data;
+//   console.log(quizData);
+// });
+// 开始页面
+function startPage() {
+    answerArea.insertAdjacentHTML('afterend','<button type="button" class="start">现在开始</button>');
+    submitBtn.style.display = 'none';
+    changeHTML('.quizName', quizData.info.name);
+    changeHTML('.quizTitle', quizData.info.main);
+    changeHTML('.answerArea',quizData.info.results);
+    document.getElementsByClassName('start')[0].addEventListener('click', loadAskAnswers,false);
+}
+// 改变元素状态和位置
+function changElement(node , classname){
+    var elements = document.querySelectorAll(node);
+    Array.prototype.forEach.call(elements, function(el, i){
+        el.classList ? el.classList.add(classname) : el.className += ' ' + classname;
+    });
+};
+function changeHTML(selecter, data){
+    var node = document.querySelectorAll(selecter)[0];
+    node.innerHTML = data;
+};
+function loadAskAnswers(){
+    var choices = quizData.questions[currentQuestion].a;
+    var choicesLength = choices.length;
+    var choicesHtml = '';
+    document.getElementsByClassName('start')[0].style.display = 'none';
+    submitBtn.style.display = 'block';
+    for (var i = 0; i < choicesLength; i++) {
+        choicesHtml += " <label class='' for='choice" + (i + 1) + "'>" + choices[i].option + "<input type='radio' name='quiz" + currentQuestion +
+        "' id='choice" + (i + 1) +
+        "' value='" + choices[i].correct + "'>"  +  "<i></i>" + "</label><br>";
+    }
+    submitBtn.addEventListener('click',checkAnswers,false);
+    changeHTML('.quizName',(currentQuestion+1)+'/'+ quizData .questions.length);
+    changeHTML('.quizTitle',quizData.questions[currentQuestion].q);
+    changeHTML('.answerArea',choicesHtml);
+    if (currentQuestion === 0) {
+        submitBtn.textContent = quizData.submitContent;
+    }
+};
+
+function checkAnswers(){
+    if (askQuestion) {
+        submitBtn.textContent = quizData.nextSubmitContent;
+        askQuestion = false;
+        // 找用户选择了哪个radio
+        var userpick;
+        var userpickCurrent;
+        var correctIndex;
+        var currentLabel;
+        var radios = document.getElementsByName('quiz' + currentQuestion);
+        var radiosLength = radios.length;
+        for (var i = 0; i < radiosLength; i++) {
+            if (radios[i].checked) {
+                userpick = radios[i].value;
+                userpickCurrent = radios[i];
+                currentLabel = document.getElementsByTagName('label')[i].style;
+            }
+            if (radios[i].value == 'true') {
+                correctIndex = i;
+            }
+        }
+        var labelCurrent = document.getElementsByTagName('label')[correctIndex];
+        var labelStyle = document.getElementsByTagName('label')[correctIndex].style;
+        if (userpick === 'true'){
+            score++;
+            var elRight = document.createElement('span');
+            labelCurrent.appendChild(elRight);
+            elRight.className = 'right';
+            labelStyle.color = '#EC6C46';
+            changElement('i','hide');
+            changElement('label','changePosition');
+        } else if (userpick === 'false') {
+            currentLabel.color = '#4A4A4A';
+            labelStyle.color = '#EC6C46';
+            changElement('i','hide');
+            changElement('label','changePosition');
+            userpickCurrent.insertAdjacentHTML('afterend','<span class="wrong"></span>');
+            var elRight = document.createElement('span');
+            labelCurrent.appendChild(elRight);
+            elRight.className = 'right';
+        }
+    } else {
+        askQuestion = true;
+        submitBtn.textContent = quizData.submitContent;
+        if (currentQuestion < quizData.questions.length - 1) {
+          currentQuestion++;
+          loadAskAnswers();
+        } else {
+          showFinalResults();
+        }
+    }
+};
+
+function showFinalResults(){
+    submitBtn.insertAdjacentHTML('afterend','<button type="button" class="Done">开启App之旅</button>');
+    var doneBtn = document.getElementsByClassName('Done')[0];
+    var scoreDescreption;
+    changeHTML('.quizName',score);
+    changeHTML('.quizTitle','最终得分');
+    scoreRange.sort(function (a,b) {
+      return a.startScore - b.startScore;
+    });
+    scoreRange.forEach(function (element) {
+      if (score >= element.startScore) {
+        scoreDescreption = element.content;
+      }
+    });
+    changeHTML('.answerArea',scoreDescreption);
+    submitBtn.style.display = 'none';
+    doneBtn.addEventListener('click', startPage,false);
+};
+
+window.addEventListener('load',startPage,false);
---
+++ src/configData.js
@@ -0,0 +1,87 @@
+var scoreRange = [
+  {
+    content: '你这个分数还行吧刚刚合格',
+    startScore: 2,
+    endScore: 3
+  },
+  {
+    content: '很不错哦!',
+    startScore: 4,
+    endScore: 5
+  },
+  {
+    content: '继续努力吧',
+    startScore: 0,
+    endScore: 1
+  }
+]
+
+
+function w() {
+  console.log('ceshi');
+}
+var quizData = {
+    "submitContent": '确定',
+    "nextSubmitContent":'下一题',
+    "info": {
+        "name":    "FANTASTIC!",
+        "main":    "恭喜您来参加测试哦!",
+        "results": "在这里你可以尽情的展示你的才华，分享你的技能；在这里你将拥有更多的掌声，更多的欢呼声；在这里你会越来越受欢迎；在这里你还可以获取更多…<br>在开始你的旅程之前，先来进行我们的问答吧！"
+    },
+    "questions": [
+        { // Question 1 - Multiple Choice, Single True Answer
+            "q": "What number is the letter A in the English alphabet?",
+            "a": [
+                {"option": "8",      "correct": false},
+                {"option": "14",     "correct": false},
+                {"option": "1",      "correct": true},
+                {"option": "23",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
+            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+        },
+        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "Which of the following best represents your preferred breakfast?",
+            "a": [
+                {"option": "Bacon and eggs",               "correct": false},
+                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
+                {"option": "Leftover pizza",               "correct": false},
+                {"option": "Eggs, fruit, toast, and milk", "correct": false} // no comma here
+            ],
+            "select_any": true,
+            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
+            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+        },
+        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
+            "q": "Where are you right now? Select ALL that apply.",
+            "a": [
+                {"option": "Planet Earth",           "correct": false},
+                {"option": "Pluto",                  "correct": false},
+                {"option": "At a computing device",  "correct": true},
+                {"option": "The Milky Way",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
+            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+        },
+        { // Question 4
+            "q": "How many inches of rain does Michigan get on average per year?",
+            "a": [
+                {"option": "149",    "correct": false},
+                {"option": "32",     "correct": true},
+                {"option": "3",      "correct": false},
+                {"option": "1291",   "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
+            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+        },
+        { // Question 5
+            "q": "Is Earth bigger than a basketball?",
+            "a": [
+                {"option": "Yes",    "correct": true},
+                {"option": "No",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
+            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+        } // no comma here
+    ]
+};
